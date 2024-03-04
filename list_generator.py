58f9a0fddafa38742144a4f2f4e6bf7a8e16a333
Mov <mamedovemil366@gmail.com>
2024-03-04 14:27:25 +0300

    change listGenerator fun

--- list_generator.py
+++ list_generator.py
@@ -1,6 +1,5 @@
-numbers = [x for x in range(10)]
-print(numbers)
-result = []
-for i in range(n):
-      element = input("Введите элемент {}: ".format(i+1))
-      result.append(element)
+def listGenerator():
+      numbers = [x for x in range(10)]
+      print(numbers)
+
+listGenerator()
